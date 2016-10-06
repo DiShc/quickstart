ae548dcb59d8c31dddf4ab80c73fb57af888b03f
Dmitriy Shvets <shvetsda@gmail.com>
2016-10-07 01:27:35 +0300

    Angular2-1 initial commit in routing implementation state

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
-
+import { HeroService} from './hero.service'
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+  selector: 'my-app',
+  template: `
+   <h1>{{title}}</h1>
+   <nav>
+     <a routerLink="/dashboard">Dashboard</a>
+     <a routerLink="/heroes">Heroes</a>
+   </nav>
+   <router-outlet></router-outlet>
+ `,
+  providers:[HeroService]
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
