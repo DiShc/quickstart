3b3464250f68fa8ea6baf04ffb63e439b02a383a
Dmitriy Shvets <shvetsda@gmail.com>
2016-11-17 00:28:58 +0200

    Angular2-3 routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
+import {NgModule}       from '@angular/core';
+import {BrowserModule}  from '@angular/platform-browser';
+import {FormsModule}    from '@angular/forms';
 
-import { AppComponent }        from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { DashboardComponent }     from './dashboard.component';
-import { HeroService }         from './hero.service';
-import { routing } from './app.routing';
+import {AppComponent}         from './app.component';
+import {HeroDetailComponent}  from './hero-detail.component';
+import {HeroesComponent}      from './heroes.component';
+import {DashboardComponent}   from './dashboard.component';
+import {HeroService}          from './hero.service';
+import {routing}              from './app.routing';
 
 @NgModule({
   imports: [
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,13 +1,16 @@
-import { Component, Input , OnInit} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
 
 import { HeroService } from './hero.service';
 import { Hero } from './hero';
+import 'rxjs/add/operator/switchMap';
+
 @Component({
   selector: 'my-hero-detail',
   moduleId: module.id,
   templateUrl: 'hero-detail.component.html',
+  styleUrls: ['hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
   hero:Hero;
@@ -17,12 +20,10 @@ export class HeroDetailComponent implements OnInit {
               private location:Location) {
   }
 
-  ngOnInit():void {
-    this.route.params.forEach((params:Params) => {
-      let id = +params['id'];
-      this.heroService.getHero(id)
-        .then(hero => this.hero = hero);
-    });
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
   }
 
   goBack():void {
